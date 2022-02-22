704ab6a4382a7c192166fd22bec0ced75138bd33
SmellyRakes <95726258+Smellyrakes@users.noreply.github.com>
2022-02-22 18:28:22 -0500

    smartDashboard the RPM value needs testing

--- src/main/java/frc/robot/commands/ShootBall.java
+++ src/main/java/frc/robot/commands/ShootBall.java
@@ -4,6 +4,7 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Shooter;
 
@@ -30,6 +31,7 @@ public class ShootBall extends CommandBase {
   @Override
   public void execute(){
     shooter.shooterRun();
+    SmartDashboard.putNumber("Shooter RPM", shooter.getRPM());
   }
 
 
